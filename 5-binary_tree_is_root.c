3e28f96f64d27199b6a4cac7181623d6e71d9068
alchemistlowkey <alchemistlowkey@gmail.com>
2023-11-01 11:55:49 -0700

    modified is root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -12,5 +12,7 @@ int binary_tree_is_root(const binary_tree_t *node)
 		return (0);
 	if (node && node->left != NULL && node->right != NULL)
 		return (1);
+	if (node->parent == NULL)
+		return (1);
 	return (0);
 }
