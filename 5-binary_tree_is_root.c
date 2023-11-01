3dfd8076e233c82f8fba53ebd0c133f482aee82d
alchemistlowkey <alchemistlowkey@gmail.com>
2023-11-01 11:35:11 -0700

    checked for root

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - A function that checks if a given node is a root
+ * @node: A pointer to the node to check
+ * Return: 0 or 1
+ */
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	if (node && node->left != NULL && node->right != NULL)
+		return (1);
+	return (0);
+}
