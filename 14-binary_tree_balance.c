887ed64cee6d70729f7f3da0a28f3f3175f03cc3
alchemistlowkey <alchemistlowkey@gmail.com>
2023-11-02 10:13:26 -0700

    balance factor measured

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,47 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - A function that measures the balance of a binary tree
+ * @tree: A pointer to the root node of the tree
+ * Return: Nothing
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int balance = 0;
+	int left_side = 0, right_side = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left)
+		left_side += 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		right_side += 1 + binary_tree_height(tree->right);
+	balance = left_side - right_side;
+	return (balance);
+}
+
+/**
+ * binary_tree_height - A function that measures the height of a binary tree
+ * @tree: A pointer to the root node of the tree
+ * Return: Nothing
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_side = 0;
+	size_t right_side = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left)
+		left_side += 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		right_side += 1 + binary_tree_height(tree->right);
+	if (right_side < left_side)
+		return (left_side);
+	else
+		return (right_side);
+}
