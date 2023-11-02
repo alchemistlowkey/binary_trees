f64a20c44282f53f5659f77ebc07bd494b88e74e
alchemistlowkey <alchemistlowkey@gmail.com>
2023-11-02 13:28:11 -0700

    modified uncle checker

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -11,8 +11,8 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (node == NULL || node->parent->parent == NULL || node->parent == NULL)
 		return (NULL);
 
-	else if (node->parent->parent->left == node->parent)
-		return (node->parent->parent->right);
-	else
+	if (node->parent->parent->right == node->parent)
 		return (node->parent->parent->left);
+	else
+		return (node->parent->parent->right);
 }
